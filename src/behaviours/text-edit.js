d0d41ecc4d9178aa7535c4c0bdff92e32164e0cf
yade <yade2@student.monash.edu>
2018-03-13 11:06:10 +1100

    Rich text editor

--- src/behaviours/text-edit.js
+++ src/behaviours/text-edit.js
@@ -4,48 +4,66 @@
  * Deals with the text edit capability of the note nodes.
  */
 const Rx = require('rxjs');
+const MediumEditor = require('medium-editor');
+require('medium-editor/dist/css/medium-editor.css');
+require('medium-editor/dist/css/themes/beagle.css');
 
 /**
  * Takes an action observable.
  */
 export default ($action, startCallback, endCallback) => {
   $action
-    .filter(action => {
-      return (action.type === 'EDITNODE' || action.type === "EDITEDGE");
-    }).map(action => {
+    .filter(action => (action.type === 'EDITNODE' || action.type === 'EDITEDGE'))
+    .map((action) => {
       startCallback && startCallback();
-      let fullRestart = action.restart;
-      let restart = () => action.restart("NOUPDATE");
-
-      let save = action.save;
-      let textElem = action.textElem;
-      let clickElem = action.clickElem;
-      let oldText, defaultText;
-      if (action.type === "EDITNODE") {
+      const fullRestart = action.restart;
+      const restart = () => action.restart('NOUPDATE');
+      const save = action.save;
+      const textElem = action.textElem;
+      const clickedElem = action.clickedElem;
+      let oldText;
+      let defaultText;
+      if (action.type === 'EDITNODE') {
         oldText = action.clickedNode.shortname;
-        defaultText = "New";
-        textElem.style.pointerEvents = "visiblePainted"; // allow mouse interaction with node text
+        defaultText = 'New';
+        textElem.style.pointerEvents = 'visiblePainted'; // allow mouse interaction with node text
       }
-      if (action.type === "EDITEDGE") {
-        defaultText = "";
+      if (action.type === 'EDITEDGE') {
+        defaultText = '';
         oldText = action.edge.predicate.text ? action.edge.predicate.text : defaultText;
       }
 
       // allow selection of text
-      textElem.classList.add("allowSelection");
+      textElem.classList.add('allowSelection');
+
+      // start rich text editor
+      const mediumEditorConfig = {
+        buttonLabels: 'fontawesome',
+        disableDoubleReturn: true,
+        toolbar: {
+          buttons: ['bold', 'italic', 'underline'],
+          diffTop: 0,
+        },
+        paste: {
+          forcePlainText: false,
+          cleanPastedHTML: false,
+        },
+        placeholder: false,
+      };
+      const editor = new MediumEditor(textElem, mediumEditorConfig);
 
       // select text if New else move caret to end
       if (window.getSelection) {
-        let range = document.createRange();
+        const range = document.createRange();
         range.selectNodeContents(textElem);
         if (textElem.innerHTML !== defaultText) {
           range.collapse(false);
         }
-        let sel = window.getSelection();
+        const sel = window.getSelection();
         sel.removeAllRanges();
         sel.addRange(range);
       } else if (document.body.createTextRange) { // IE compatability
-        let textRange = document.body.createTextRange();
+        const textRange = document.body.createTextRange();
         textRange.moveToElementText(textElem);
         if (textElem.innerHTML !== defaultText) {
           textRange.collapse(false);
@@ -54,33 +72,48 @@ export default ($action, startCallback, endCallback) => {
       }
 
       textElem.focus(); // focus on text box
-      // textElem.innerText = textElem.innerHTML.replace(/<br>/g, "\n").replace(/&nbsp;/g, " "); // display HTML content of node //TODO-ya determine how interaction will work
       restart();
 
+      // Input handling
+      const $input = Rx.Observable.fromEventPattern(
+        handler => editor.subscribe('editableInput', handler),
+        handler => editor.unsubscribe('editableInput', handler),
+      );
       // correct behaviour on paste
-      Rx.Observable.fromEvent(textElem, "paste")
-        .do(e => {
+      const $paste = Rx.Observable.fromEvent(textElem, 'paste')
+        .do((e) => {
           e.preventDefault();
-          document.execCommand("insertText", false, e.clipboardData.getData('text/plain'));
-        }).subscribe(restart);
+          const HTMLText = e.clipboardData.getData('text/html').replace(/[\n\r]+/g, '\n');
+          const plaintext = e.clipboardData.getData('text/plain');
+          editor.cleanPaste(HTMLText || plaintext);
+        });
 
-      // Exit on "esc" keypress
-      let $esc = Rx.Observable.concat(
-        Rx.Observable.fromEvent(document, "keyup")
-          .filter(e => e.keyCode == 27)
-      );
-      // Exit on mouseclick
-      let $mouseClick = Rx.Observable.concat(
-        Rx.Observable.fromEvent(document, "click")
+      // Exit Handling
+      // On "esc" keypress cancel changes
+      const $esc = Rx.Observable.concat(
+        Rx.Observable.fromEvent(document, 'keyup')
+          .filter(e => e.keyCode === 27)
+          .do(() => {
+            textElem.innerHTML = oldText;
+          }));
+      // On mouseclick
+      const $mouseClick = Rx.Observable.concat(
+        Rx.Observable.fromEvent(document, 'click')
           .filter(e => e.target !== textElem)
-      );
+          .map(e => ({ e, descendants: Array.from(textElem.querySelectorAll('*')) }))
+          .filter(({ e, descendants }) => !descendants.includes(e.target)));
       // Exit on focus lost
-      let blur = Rx.Observable.fromEvent(textElem, "blur");
+      const $editorBlur = Rx.Observable.fromEventPattern(
+        handler => editor.subscribe('blur', handler),
+        handler => editor.unsubscribe('blur', handler),
+      );
+      const $windowBlur = Rx.Observable.fromEvent(window, 'blur');
       // on exit
-      let $exit = Rx.Observable.merge(
+      const $exit = Rx.Observable.merge(
         $esc,
         $mouseClick,
-        blur,
+        $windowBlur,
+        $editorBlur,
       ).do(() => {
         // remove selection
         if (window.getSelection) {
@@ -88,56 +121,37 @@ export default ($action, startCallback, endCallback) => {
         } else if (document.selection) {
           document.selection.empty();
         }
-        //remove focus
+        // remove focus
         document.activeElement.blur();
         // remove text selection
-        textElem.classList.remove("allowSelection");
-        if (action.type === "EDITNODE") {
-          textElem.style.pointerEvents = "none";
+        textElem.classList.remove('allowSelection');
+        if (action.type === 'EDITNODE') {
+          textElem.style.pointerEvents = 'none';
         }
       });
 
-
-      // Letters
-      let $letters = Rx.Observable.fromEvent(document, "keyup");
-      // .filter(e => e.keyCode !== 8 && e.keyCode !== 13)
-      // .do((e) => {
-      //   // if ((e.keyCode || e.which) === 32) {
-      //   //   e.preventDefault();
-      //   // }
-      //   // let event = e || window.event;
-      //   // let char = String.fromCharCode(e.keyCode || e.which)
-      //   // text.addLetter(char);
-      // })
-      // .do(restart);
-
-      // Return the typing observables merged together.
-      let $typingControls = Rx.Observable.merge(
-        $letters,
-      ).do(() => {
-        restart();
-      }).takeUntil($exit)
+      // Return the typing observable.
+      const $typingControls = Rx.Observable
+        .merge($paste, $input)
+        .do(restart)
+        .takeUntil($exit)
         .finally(() => {
-          // textElem.innerText = textElem.innerText.trim().replace(/\n/g, "<br>");
-          // textElem.innerText = textElem.innerHTML;
-          if (!textElem.innerText || textElem.innerText === '' || textElem.innerText.length === 0 ||
-            textElem.innerText[0] === '' || textElem.innerText === oldText) {
-            textElem.innerText = oldText;
+          editor.destroy();
+          if (!textElem.innerHTML || textElem.innerHTML === '' ||
+            textElem.innerHTML.length === 0 || textElem.innerHTML === oldText) {
+            textElem.innerHTML = oldText;
             fullRestart();
           } else {
-            save && save(textElem.innerText);
+            save && save(textElem.innerHTML);
           }
           endCallback && endCallback();
         });
       return $typingControls;
-    }
-  )
+    })
     .switch()
     .subscribe(
-      () => {
-      },
-      // console.log,
+      () => undefined,
       console.error,
-      () => console.log("FINISH")
+      () => console.log('FINISH'),
     );
-}
+};

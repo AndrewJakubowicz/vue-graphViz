f921a7323755e6b7b8eb4f683f8f37eb192ce9db
yade <yade2@student.monash.edu>
2018-07-27 16:54:49 +1000

    change default colours

--- src/graphViz.vue
+++ src/graphViz.vue
@@ -11,6 +11,7 @@
     <photoshop-picker v-show="ifColorPickerOpen"
                       :style="styleObject"
                       :value="colors"
+                      :palette="palette"
                       @input="updateFromPicker"
                       ref="vueColorPicker">
     </photoshop-picker>
@@ -83,6 +84,11 @@
         colors: {
           hex: '#FFFFFF',
         },
+        palette:[
+          '#4D4D4D', '#999999', '#FFFFFF', '#F44E3B', '#FE9200', '#F6ECAF', '#DBDF00', '#A4DD00', '#AADCDC', '#73D8FF', '#AEA1FF', '#FDA1FF',
+          '#333333', '#808080', '#CCCCCC', '#D33115', '#E27300', '#FCDC00', '#B0BC00', '#68BC00', '#16A5A5', '#009CE0', '#7B64FF', '#FA28FF',
+          '#000000', '#666666', '#B3B3B3', '#9F0500', '#C45100', '#FCC400', '#808900', '#194D33', '#0C797D', '#0062B1', '#653294', '#AB149E'
+        ],
         graph: undefined,
         nodesOutsideDiagram: [],
         mouseState: POINTER,
@@ -418,7 +424,7 @@
                       img: false,
                       isSnip: false,
                       fixed: true,
-                      color: '#ffffff',
+                      color: '#aadcdc',
                       fixedWidth: false,
                     };
                     const textNode = Object.assign({}, defaultNode, n);
